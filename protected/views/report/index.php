05a43a4762d19a6567743ce7b414154dcd59f879
asuch <veritablefake@gmail.com>
2016-05-11 19:01:44 +0300

    test1

--- protected/views/report/index.php
+++ protected/views/report/index.php
@@ -13,6 +13,6 @@ $this->menu = array(
         'url' => $this->createUrl('MakeGroupList'),
     ),
 );
-echo CHtml::dropDownList('listname', 'empty',  array('sdss'=>'afsafss'), array('empty' => '(Select a teacher)'));
+echo CHtml::dropDownList('listname', 'empty',  array('sdss'=>'afsafssa'), array('empty' => '(Select a teacher)'));
 echo CHtml::dropDownList('listname', 'empty',  Teacher::getList(), array('empty' => '(Select a teacher)'));
 ?>
